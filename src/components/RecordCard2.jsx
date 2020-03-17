59b5c3b764a44501c46fc55cbca7ee94172bdaa3
reedvoid <reedvoid@gmail.com>
2020-03-16 18:11:01 -0700

    removed old recordcard

--- src/components/RecordCard2.jsx
+++ src/components/RecordCard2.jsx
@@ -12,10 +12,10 @@ import Box from "@material-ui/core/Box";
 
 import Chip from "@material-ui/core/Chip";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
-import CheckIcon from "@material-ui/icons/Check";
 import PriorityHighIcon from "@material-ui/icons/PriorityHigh";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import IconButton from "@material-ui/core/IconButton";
+import CardActionArea from "@material-ui/core/CardActionArea";
 
 const useStyles = makeStyles({
   root: {
@@ -54,57 +54,59 @@ export default function RecordCard() {
 
   return (
     <Card className={classes.root}>
-      <CardContent>
-        <Grid container spacing={4}>
-          <Grid item xs={2}>
-            <Avatar variant="square" src="https://i.imgur.com/IbZv075.png" />
-          </Grid>
+      <CardActionArea>
+        <CardContent>
+          <Grid container spacing={4}>
+            <Grid item xs={2}>
+              <Avatar variant="square" src="https://i.imgur.com/IbZv075.png" />
+            </Grid>
 
-          <Grid item xs={7}>
-            <Typography component="div">
-              <Box className={classes.record_title}>
-                Louver market street west side...
-              </Box>
-            </Typography>
-          </Grid>
-
-          <Grid item xs={3}>
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={4}>
-          <Grid item xs={2} className={classes.grid_record_details_item} />
-
-          <Grid item xs={10} className={classes.grid_record_details_item}>
-            <Typography component="div">
-              <Box className={classes.record_short_detail_wrapper}>
-                <Box component="span" mr={1}>
-                  2 hours ago
+            <Grid item xs={7}>
+              <Typography component="div">
+                <Box className={classes.record_title}>
+                  Louver market street west side...
                 </Box>
-                <Box component="span" mr={1}>
-                  <Chip
-                    className={classes.record_chip}
-                    size="small"
-                    icon={<AttachFileIcon />}
-                    label="2"
-                  />
-                </Box>
-                <Box component="span" mr={1}>
-                  <Chip
-                    size="small"
-                    color="secondary"
-                    icon={<PriorityHighIcon />}
-                    label="3/4"
-                  />
-                </Box>
-              </Box>
-            </Typography>
+              </Typography>
+            </Grid>
+
+            <Grid item xs={3}>
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            </Grid>
           </Grid>
-        </Grid>
-      </CardContent>
+
+          <Grid container spacing={4}>
+            <Grid item xs={2} className={classes.grid_record_details_item} />
+
+            <Grid item xs={10} className={classes.grid_record_details_item}>
+              <Typography component="div">
+                <Box className={classes.record_short_detail_wrapper}>
+                  <Box component="span" mr={1}>
+                    2 hours ago
+                  </Box>
+                  <Box component="span" mr={1}>
+                    <Chip
+                      className={classes.record_chip}
+                      size="small"
+                      icon={<AttachFileIcon />}
+                      label="2"
+                    />
+                  </Box>
+                  <Box component="span" mr={1}>
+                    <Chip
+                      size="small"
+                      color="secondary"
+                      icon={<PriorityHighIcon />}
+                      label="3/4"
+                    />
+                  </Box>
+                </Box>
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
